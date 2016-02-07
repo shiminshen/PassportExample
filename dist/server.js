868b9305bf5e071b8d70643a11478c0ff4ce99b7
smshen <thanatos1710@gmail.com>
2016-02-07 18:49:14 +0800

    fix bug to get login information

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var flash = require('connect-flash');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var open = require('open');
@@ -9,38 +10,45 @@ var port = 3000;
 passport.use(new FacebookStrategy({
   clientID: '1505414496431853',
   clientSecret: '10a20b4913227b7e958700673680695a',
-  callbackURL: "http://localhost:3000/",
+  callbackURL: "http://localhost:3000/login/",
   profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
 },
 function(accessToken, refreshToken, profile, cb) {
-  return done(null, profile);
+  return cb(null, profile);
 }));
 
 app.use(express.static('.'))
 .use(require('cookie-parser')())
 .use(require('body-parser').urlencoded({ extended: true  }))
-.use(require('express-session')({ secret: 'smshen', resave: true, saveUninitialized: true  }))
+.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true  }))
 .use(passport.initialize())
 .use(passport.session());
 
+
 passport.serializeUser(function(user, done) {
-  console.log(user);
   done(null, user);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+  // User.findById(id, function(err, user) {
+  //   done(err, user);
+  // });
 });
 
 app.get('/showInfo', function(req, res) {
-  
 });
 
-app.get('/login', passport.authenticate('facebook'));
+app.get('/login', passport.authenticate('facebook', {failureRedirect: '/failed'})
+, function (req, res) {
+  console.log('Login success!');
+  console.log(req.user);
+  res.redirect('/');
+});
 
 app.get('/logout', function(req, res) {
+  console.log(req.isAuthenticated());
+  console.log(req.user);
   req.logout();
   res.clearCookie('connect.sid', { path: '/' });
   req.session.destroy((err) => {
